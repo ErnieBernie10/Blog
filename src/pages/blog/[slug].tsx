11e054244884d3a4a7bed0283ec179c2e6ff6dcc
ErnieBernie10 <arne.boedt1@gmail.com>
2021-11-09 21:45:27 +0100

    Added tailwind and eslint config

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -5,33 +5,33 @@ import { getPostForSlug, getPosts } from "../../api/getPosts";
 import { Post } from "../../api/models/models";
 
 const PostPage: NextPage<{ post: Post }> = ({ post }) => {
-	const router = useRouter();
-	if (router.isFallback) {
-		return <>Loading...</>
-	}
-	return (
-		<article>
-			<h2>{post.title}</h2>
-			{post.content && <ReactMarkdown>{post.content}</ReactMarkdown>}
-		</article>
-	);
-}
+  const router = useRouter();
+  if (router.isFallback) {
+    return <>Loading...</>;
+  }
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      {post.content && <ReactMarkdown>{post.content}</ReactMarkdown>}
+    </article>
+  );
+};
 
-export const getStaticProps: GetStaticProps = async ({ params, preview = null }) => {
-	const { data: post } = await getPostForSlug(params?.slug as string);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { data: post } = await getPostForSlug(params?.slug as string);
   return {
     props: {
-			post
+      post,
     },
-  }
-}
+  };
+};
 
 export const getStaticPaths = async () => {
-	const { data: allPosts } = await getPosts();
+  const { data: allPosts } = await getPosts();
   return {
     paths: allPosts?.map((post) => `/blog/${post.slug}`) || [],
     fallback: true,
-  }
-}
+  };
+};
 
 export default PostPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
-import type { GetStaticProps, NextPage } from 'next'
+import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
-import { getPosts } from '../api/getPosts'
-import { definitions } from '../api/models/supabase'
+import { getPosts } from "../api/getPosts";
+import { definitions } from "../api/models/supabase";
 
-const Home: NextPage<{ posts: Array<definitions["posts"]> }> = ({ posts }) => {
-  return (
-    <main>
-      {posts.map((p, i) => (<div key={i}><h3>{p.title}</h3><Link href={`/blog/${p.slug}`}>Read more</Link></div>))}
-    </main>
-  )
-}
+const Home: NextPage<{ posts: Array<definitions["posts"]> }> = ({ posts }) => (
+  <main>
+    {posts.map((p, i) => (
+      <div key={i}>
+        <h3>{p.title}</h3>
+        <Link href={`/blog/${p.slug}`}>Read more</Link>
+      </div>
+    ))}
+  </main>
+);
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async () => {
   const posts = await getPosts();
   return {
     props: {
-      posts: posts.data
-    }
-  }
-}
+      posts: posts.data,
+    },
+  };
+};
 
-export default Home
+export default Home;
