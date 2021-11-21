14f681971f34694f0101404a9aa3107be5be5505
ErnieBernie10 <arne.boedt1@gmail.com>
2021-11-21 16:18:59 +0100

    Top menu bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { Navbar } from '../components/Navbar';
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <div className="font-serif">
+      <Navbar />
       <main className="container mx-auto ">
         <Component {...pageProps} />
       </main>
